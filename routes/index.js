1638759e1578f283e7d2a3d14a296cbf63331ffe
Alexander Korolev <kilork@yandex.ru>
2017-08-01 09:24:56 +0300

    Updated styles.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', letters: 'ᚠᚢᚦᚨᚱᚲᚷᚹᚺᚻ'.split('').map((letter) => { return {letter: letter}; }) }); //ᚾᛁᛃᛇᛈᛉᛊᛋᛏᛒᛖᛗᛚᛜᛝᛟᛞ
+  // ᚠᚡᚢᚣᚤᚥᚦᚧᚨᚩᚪᚫᚬᚭᚮᚯ
+  // ᚰᚱᚲᚳᚴᚵᚶᚷᚸᚹᚺᚻᚼᚽᚾᚿ
+  // ᛀᛁᛂᛃᛄᛅᛆᛇᛈᛉᛊᛋᛌᛍᛎᛏ
+  // ᛐᛑᛒᛓᛔᛕᛖᛗᛘᛙᛚᛛᛜᛝᛞᛟ
+  // ᛠᛡᛢᛣᛤᛥᛦᛧᛨᛩᛪ᛫᛬᛭ᛮᛯ
+  // ᛰ
 });
 
 module.exports = router;
