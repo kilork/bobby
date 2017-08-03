c4be4545e5eda17238a43aaf8b03718608d87969
Alexander Korolev <kilork@yandex.ru>
2017-08-03 08:50:13 +0300

    Added terminal to UI.

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
+const debug = require('debug')('bobby:app');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 let api = require('./routes/api');
 
 var app = express();
+require('express-ws')(app);
+
+const pty = require('node-pty');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,6 +37,72 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/api/', api)
+
+let terminals = {};
+let logs = {};
+
+app.post('/terminals', (req, res) => {
+  debug('received POST request to /terminals with body: %o', req.body);
+  let cols = req.body.cols || 80;
+  let rows = req.body.rows || 24;
+  if (cols == 0) {
+    cols = 80;
+  }
+  if (rows == 0) {
+    rows = 24;
+  }
+  let term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
+    name: 'xterm-color',
+    cols: cols,
+    rows: rows,
+    cwd: process.env.CWD,
+    env: process.env
+  });
+  debug(`Created terminal with PID: ${term.pid}`);
+  let sPID = term.pid.toString();
+  terminals[sPID] = term;
+  logs[sPID] = '';
+  term.on('data', (data) => logs[sPID] += data);
+  req.body.pid = sPID;
+  res.send(req.body);
+  res.end();
+});
+
+app.put('/terminals/:pid', (req, res) => {
+  debug('received PUT request to /terminals/%s with body: %o', req.params.pid, req.body);
+  let term = terminals[req.params.pid];
+  let cols = req.body.cols || 80;
+  let rows = req.body.rows || 24;
+  term.resize(cols, rows);
+  debug('terminal %s resized to %d cols %d rows', req.params.pid, cols, rows);
+  res.end();
+});
+
+app.ws('/terminals/:pid', (ws, req) => {
+  let pid = req.params.pid;
+  debug('received WS request to /terminals/%s', pid);
+  let term = terminals[pid];
+  if (!term) {
+    debug('not found term!');
+  }
+  ws.send(logs[pid]);
+
+  term.on('data', (data) => {
+    try {
+      ws.send(data);
+    } catch (ex) {
+      // The WebSocket is not open, ignore
+    }
+  });
+  ws.on('message', (msg) => term.write(msg));
+  ws.on('close', () => {
+    term.kill();
+    debug('closed terminal %s', pid);
+    // cleanup
+    delete terminals[pid];
+    delete logs[pid];
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
